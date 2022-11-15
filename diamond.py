efb81671417cc58271071cb770cb83e4755f815b
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-15 22:42:32 +0100

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,2 +1,39 @@
 def rows(letter):
-    pass
+    pos = ord(letter) - ord('A')
+    
+    num_rows = pos
+    middle_space = 0
+    row_letter = 'A'
+    
+    diamond_list = []
+    
+    for i in range(pos + 1):
+        cur_str = []
+        for j in range(pos):
+            cur_str = cur_str + ' '
+         
+        cur_str = cur_str + row_letter
+        
+        for k in range(middle_space):
+            cur_str = cur_str + ' '
+        
+        if i != 0:
+            cur_str = cur_str + row_letter
+            
+        for j in range(pos):
+            cur_str = cur_str + ' '
+            
+        pos = pos - 1
+        middle_space = middle_space + 2
+        
+        diamond_list.append(cur_str)
+    
+    j = len(diamond_list) - 2
+    for i in range(num_rows):
+        diamond_list-append(diamond_list[j])
+        
+    return diamond_list
+        
+            
+            
+        
